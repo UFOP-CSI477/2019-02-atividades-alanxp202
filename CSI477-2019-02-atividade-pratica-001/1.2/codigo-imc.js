fc252f17909ef42168cb3cab69747191067c4b4e
alanxp202 <36681485+alanxp202@users.noreply.github.com>
2019-10-26 12:39:01 -0300

    Atividade 1.2 concluida

--- CSI477-2019-02-atividade-pratica-001/1.2/codigo-imc.js
+++ CSI477-2019-02-atividade-pratica-001/1.2/codigo-imc.js
@@ -1,10 +1,33 @@
-function calcular(){
+function calcular() {
     let altura = document.dados.infoAlt;
     let peso = document.dados.infoPeso;
 
-    let resultado = (peso.value/(altura.value*altura.value));
-    
-    document.dados.imc.value = resultado;
-    console.log(resultado.value);
+    let resultado = (peso.value / Math.pow(altura.value, 2));
+    let pesoMin = (Math.pow(altura.value, 2) * 18.5);
+    let pesoMax = (Math.pow(altura.value, 2) * 24.9);
+
+    if (isNaN(resultado)) {
+
+        document.dados.pesoIdeal.value = "Digite um valor numérico!";
+        document.dados.situacao.value = "Digite um valor numérico!";
+        document.dados.imc.value = "Digite um valor numérico!";
+        return;
+    }
+
+    document.dados.pesoIdeal.value = ("Peso ideal entre " + pesoMin.toPrecision(2) + " e " + pesoMax.toPrecision(2) + " kg");
+    document.dados.imc.value = resultado.toPrecision(2);
+    if (resultado < 18.5) {
+        document.dados.situacao.value = "Subnutrição!";
+    } else if (resultado >= 18.5 && resultado < 25) {
+        document.dados.situacao.value = "Peso Saldável!";
+    } else if (resultado >= 25 && resultado < 30) {
+        document.dados.situacao.value = "Sobrepeso!";
+    } else if (resultado >= 30 && resultado < 35) {
+        document.dados.situacao.value = "Obesidade grau 1!";
+    } else if (resultado >= 35 && resultado < 40) {
+        document.dados.situacao.value = "Obesidade grau 2!";
+    } else {
+        document.dados.situacao.value = "Obesidade grau 3!";
+    }
 
 }
