8c354651f79f101704f24107236217ca6285cab4
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2019-11-05 22:08:10 -0300

    Laravel

---
+++ Codes/004-php/laravel/academico/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    //return view('welcome');
+    return "Olá mundo!";
+});
+
+Route::get('/listar', function() {
+    $lista = ['Ana', 'Bruna', 'Hugo', 'João', 'Pedro'];
+    return $lista;
+});
