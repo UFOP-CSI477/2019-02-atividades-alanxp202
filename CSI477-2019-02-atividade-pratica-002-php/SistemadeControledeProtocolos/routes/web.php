983da97c6512f6232b1cc399e600299b68b3570a
alanxp202 <36681485+alanxp202@users.noreply.github.com>
2019-12-03 23:37:22 -0300

    Atividade 002 - 0.2

--- CSI477-2019-02-atividade-pratica-002-php/SistemadeControledeProtocolos/routes/web.php
+++ CSI477-2019-02-atividade-pratica-002-php/SistemadeControledeProtocolos/routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'PrincipalController@index');
+
+Route::get('/contato', function(){
+    return "Contato";
 });
