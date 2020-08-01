34269b20b180258ff50bacf2f9560e74fb5e40e2
alanxp202 <36681485+alanxp202@users.noreply.github.com>
2020-08-01 12:32:52 -0300

    Update de seguranca

--- CSI477-2019-02-atividade-pratica-002-php/SistemadeControledeProtocolos/database/migrations/2019_12_06_005837_create_subjects_table.php
+++ CSI477-2019-02-atividade-pratica-002-php/SistemadeControledeProtocolos/database/migrations/2019_12_06_005837_create_subjects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSubjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('subjects', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->tinyInteger('type');
-            $table->rememberToken();
+            $table->string('name',60);
+            $table->float('price',10,2);
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('subjects');
     }
 }
---
+++ CSI477-2019-02-atividade-pratica-002-php/SistemadeControledeProtocolos/resources/views/principal/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('principal.layout.footer')
+
+@extends('principal.layout.content')
+@extends('principal.layout.content')
+@extends('principal.layout.content')
+
+@extends('principal.layout.header')
+@section('titulo', 'Quero Livros!')
+@section('botao.menu', 'Login')
